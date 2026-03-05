c1063e2bae0d51a6ff0b05c4ca8d6972bd84d9e5
Reyansh <piu4photo@gmail.com>
2026-03-05 18:54:09 -0500

    Add files via upload

--- jumblewrdo.py
+++ jumblewrdo.py
@@ -15,4 +15,21 @@ def playgame():
     rounds=7
     print("Welcome to the Word Jumble Game!")
     print("Unscramble the letters to find the word. There are 7 rounds. Get as many correct answers as you can!") 
-    
+    for i in range(1, rounds+1):
+        word=random.choice(words)
+        jumbledword=wordjumble(word)
+        print("Round {}: {}".format(i, jumbledword))
+        hint=input("Do you want a hint? Type yes or no").lower()
+        if hint =="yes":
+            print(givehint(word))
+        guess=input("What is the word?").lower()
+        while not guess.isalpha():
+            print("That is not a word. Please try again.")
+            guess=input("What is the word?").lower()
+        if guess == word:
+            print("Correct!")
+            score+=1
+        else:
+            print("Wrong. The word was {}".format(word))
+    print("You made it! All 7 rounds complete. Your score is {} out of 7".format(score))
+playgame()
